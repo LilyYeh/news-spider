5bd08431b3cb4c47ca97c09c789fa015fec4b09f
Lily Yeh <lilyyeh@LilydeMacBook-Air.local>
2025-06-04 16:46:05 +0800

    ETtoday 新聞區塊 style

--- static/main.js
+++ static/main.js
@@ -1,10 +1,22 @@
-function setTab(category) {
-    sessionStorage.setItem('selectedTab', category);
-}
-
 $(document).ready(function () {
     // 頁面載入後，填入更新時間
     $('.last-update-time span').text(nowDateTime());
+
+    function nowDateTime() {
+        const now = new Date();
+        const taipeiTime = new Date(
+            now.toLocaleString("en-US", {timeZone: "Asia/Taipei"})
+        );
+
+        const year = taipeiTime.getFullYear();
+        const month = String(taipeiTime.getMonth() + 1).padStart(2, '0');
+        const day = String(taipeiTime.getDate()).padStart(2, '0');
+        const hours = String(taipeiTime.getHours()).padStart(2, '0');
+        const minutes = String(taipeiTime.getMinutes()).padStart(2, '0');
+        const seconds = String(taipeiTime.getSeconds()).padStart(2, '0');
+
+        return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+    }
 
     // 頁面載入後設定選中的 Tab
     const tab = sessionStorage.getItem('selectedTab');
@@ -31,48 +43,42 @@ $(document).ready(function () {
         }
     });
 
-    function nowDateTime() {
-        const now = new Date();
-        const taipeiTime = new Date(
-            now.toLocaleString("en-US", {timeZone: "Asia/Taipei"})
-        );
 
-        const year = taipeiTime.getFullYear();
-        const month = String(taipeiTime.getMonth() + 1).padStart(2, '0');
-        const day = String(taipeiTime.getDate()).padStart(2, '0');
-        const hours = String(taipeiTime.getHours()).padStart(2, '0');
-        const minutes = String(taipeiTime.getMinutes()).padStart(2, '0');
-        const seconds = String(taipeiTime.getSeconds()).padStart(2, '0');
-
-        return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
-    }
-
-
-    let isOpen = false;
+    let isOpen = sessionStorage.getItem('isOpenEttodayWrapper');
     const ettodayWrapper = '.ettoday-wrapper';
     const collapseBtn = '.collapse-btn';
-    const collapseWrapper = '.collapse-wrapper';
+
+    switchCollapseWrapper();
 
     $(collapseBtn).on('click', function () {
-        switchCollapseWrapper();
+        if (isOpen === 'false') {
+            openCollapseWrapper();
+            isOpen = 'true';
+            sessionStorage.setItem('isOpenEttodayWrapper', 'true');
+        } else {
+            closeAllCollapseWrapper();
+            isOpen = 'false';
+            sessionStorage.setItem('isOpenEttodayWrapper', 'false');
+        }
     });
 
     function switchCollapseWrapper() {
-        if (isOpen) {
-            openCollapseWrapper();
-            isOpen = false;
-        } else {
+        if (isOpen === 'false') {
             closeAllCollapseWrapper();
-            isOpen = true;
+        } else {
+            openCollapseWrapper();
         }
     }
 
     function openCollapseWrapper() {
         $(ettodayWrapper).removeClass('collapsed');
-        //$(collapseWrapper).css('height', 'auto');
     }
 
     function closeAllCollapseWrapper() {
         $(ettodayWrapper).addClass('collapsed')
     }
 });
+
+function setTab(category) {
+    sessionStorage.setItem('selectedTab', category);
+}
